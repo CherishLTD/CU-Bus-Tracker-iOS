4060c8603f1d3c0d59bf98a76c12f9fc655e7c81
nick-smith8 <poweller8@comcast.net>
2015-07-19 12:49:48 -0600

    Added case switch for routes

--- BuffBus4/OptionsViewController.swift
+++ BuffBus4/OptionsViewController.swift
@@ -31,22 +31,35 @@ class OptionsViewController: UIViewController {
         hopCCButton.addTarget(self, action: "buttonClicked:", forControlEvents: .TouchUpInside)
         buffButton.tag = 1
         buffButton.addTarget(self, action: "buttonClicked:", forControlEvents: .TouchUpInside)
+        lnbButton.tag = 4
+        lnbButton.addTarget(self, action: "buttonClicked:", forControlEvents: .TouchUpInside)
+        lnsButton.tag = 5
+        lnsButton.addTarget(self, action: "buttonClicked:", forControlEvents: .TouchUpInside)
+        lngButton.tag = 3
+        lngButton.addTarget(self, action: "buttonClicked:", forControlEvents: .TouchUpInside)
+        
     }
     
     
     func buttonClicked( sender: AnyObject?) {
-    print("Hello world")
-    //presentViewController(ViewController(), animated: true, completion: nil)
-        if sender!.tag == 6 {
-            routeNumber = 6
-        }
-        if sender!.tag == 7 {
-            routeNumber = 7
-        }
-        if sender!.tag == 1 {
+
+        switch sender!.tag {
+        case 1:
             routeNumber = 1
+        case 6:
+            routeNumber = 6
+        case 7:
+            routeNumber = 7
+        case 4:
+            routeNumber = 4
+        case 5:
+            routeNumber = 5
+        case 3:
+            routeNumber = 3
+        default:
+            fatalError("The route numbers didn't match")
         }
-    performSegueWithIdentifier("mainSegue", sender: self)
+        performSegueWithIdentifier("mainSegue", sender: self)
 
     }
     
