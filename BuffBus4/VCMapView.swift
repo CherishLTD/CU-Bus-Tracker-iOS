de78ca37b83f6136b14ff768f0e7eefdfb60a603
nick-smith8 <poweller8@comcast.net>
2015-07-22 20:43:24 -0600

    moved select stop

--- BuffBus4/VCMapView.swift
+++ BuffBus4/VCMapView.swift
@@ -9,26 +9,31 @@
 import Foundation
 import MapKit
 import UIKit
-
+import CoreLocation
 
 extension ViewController: MKMapViewDelegate {
     
+    
     func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
-        println("WOOOOOO")
-        if let annotation = annotation as? Bus {
-            let identifier = "pin"
-            var view: MKPinAnnotationView
-            if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
-                as? MKPinAnnotationView {
-                dequeuedView.annotation = annotation
-                view = dequeuedView
-            }
-            else {
-                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        
+        
+        if let annotation = annotation as? MKAnnotation  {
+            
+//            var loc1 = CLLocation(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
+//            var loc2 = CLLocation(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
+//            
+//            
+//            let distance = loc1.distanceFromLocation(loc2)
+//            println(distance)
+            
+            let identifier = "test"
+            var view: MKAnnotationView
+
+                view = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
                 view.canShowCallout = true
                 var image: UIImage? = UIImage(named: "nikcy.jpg")
                 if image != nil {
-                    println("yay")
+                    //println("yay")
                 }
                 
                 view.image = UIImage(named: "nikcy.jpg")
@@ -37,11 +42,11 @@ extension ViewController: MKMapViewDelegate {
                 view.calloutOffset = CGPoint(x: -5, y: 5)
                 view.rightCalloutAccessoryView = UIButton.buttonWithType(.DetailDisclosure) as! UIView
         
-            println("end")
             return view
         }
         return nil
     }
+    
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         initialLocation = locations[0] as! CLLocation
